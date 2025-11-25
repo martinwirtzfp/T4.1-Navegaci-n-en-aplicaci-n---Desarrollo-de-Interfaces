5a72f1dab883cb0f9437d3d73afaf8c984fef915
martin.sancheznovo <martinwirtzfp@gmail.com>
2025-11-25 20:22:37 +0100

    cambios en la lógica

--- tarea_4_1_interfaces/lib/viewmodels/ordersViewModel.dart
+++ tarea_4_1_interfaces/lib/viewmodels/ordersViewModel.dart
@@ -7,8 +7,6 @@ class Orderviewmodel extends ChangeNotifier {
   // Solo accesible desde este ViewModel
   final List<Order> orders = [];
 
- 
-
   // Crear pedidos iniciales
   void loadInitialOrders() {
     orders.addAll([
@@ -20,8 +18,13 @@ class Orderviewmodel extends ChangeNotifier {
 
   // Añadir un pedido
   void addOrder(Order order) {
-    orders.add(order);
-    notifyListeners();
+    for (var o in orders) {
+      if (o.table == order.table) {
+        return;
+      }
+      orders.add(order);
+      notifyListeners();
+    }
   }
 
   // Eliminar un pedido
@@ -31,10 +34,7 @@ class Orderviewmodel extends ChangeNotifier {
   }
 
   // Añadir un producto a un pedido
-  void addProductToOrder(int table, Product product, int quantity) {
-    
-  }
+  void addProductToOrder(int table, Product product, int quantity) {}
 
   // Eliminar un producto de un pedido
-  
 }
